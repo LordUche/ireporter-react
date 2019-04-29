5a22956d70147e61798ded460091c551e54fe447
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-04-29 13:34:26 +0100

    feat(navbar-sidebar): add navbar and sidebar components

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Container } from 'semantic-ui-react';
 import Routes from '../Routes';
 import Navbar from './Navbar';
+import Sidenav from './Sidenav';
 
-export const App = ({ login }) => {
-  return (
-    <main>
-      <Navbar loggedIn={login.loggedIn} />
-      <Routes />
-    </main>
-  );
-};
+export class App extends React.Component {
+  state = { sidebarOpen: false };
+
+  handleSidebarShow = () => this.setState({ sidebarOpen: true });
+
+  handleSidebarHide = () => this.setState({ sidebarOpen: false });
+
+  render() {
+    const { login } = this.props;
+    const { sidebarOpen } = this.state;
+    return (
+      <Router>
+        <Sidenav visible={sidebarOpen} hideSidebar={this.handleSidebarHide}>
+          <main>
+            <Navbar
+              loggedIn={login.loggedIn}
+              sidebarOpen={sidebarOpen}
+              openSidebar={this.handleSidebarShow}
+              closeSidebar={this.handleSidebarHide}
+            />
+            <Container className="main-content">
+              <Routes />
+            </Container>
+          </main>
+        </Sidenav>
+      </Router>
+    );
+  }
+}
 
 App.propTypes = {
   login: PropTypes.shape({ loading: PropTypes.bool, loggedIn: PropTypes.bool })
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Header } from 'semantic-ui-react';
 
 const Landing = () => {
-  return <div>Welcome to iReporter</div>;
+  return (
+    <div className="landing">
+      <Header as="h1" className="landing__header">
+        See something? <br /> Report it!
+      </Header>
+      <Header.Subheader className="landing__subheader">
+        <span>
+          Say <span className="color-red">NO</span> to corruption
+        </span>
+        <br />
+        <span>Be the voice in your community</span>
+      </Header.Subheader>
+    </div>
+  );
 };
 
 export default Landing;
